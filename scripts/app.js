7e8c3b75afd2e2da07e3be14a70302b761bbe868
1cito <j@1cito.com>
2015-07-25 23:32:51 -0400

    Initial check in

---
+++ scripts/app.js
@@ -0,0 +1,142 @@
+﻿/**
+ *  
+ */
+
+//'use strict';
+
+//----------------------------------------//
+var appMainModule = angular.module('appMain', ['ui.router', 'ngAnimate']);
+
+appMainModule.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    var baseurl = '/';
+
+    $urlRouterProvider.otherwise(baseurl);
+
+    $locationProvider.html5Mode(true);
+
+    $stateProvider
+	// home
+	.state('index', {
+		url: baseurl,
+		templateUrl: 'templates/home.html',
+		controller: 'homeViewModel'
+	})
+
+    $stateProvider
+	// home
+	.state('home', {
+		url: baseurl + 'home',
+		templateUrl: 'templates/home.html',
+		controller: 'homeViewModel'
+	})
+
+	// events
+    .state('events', {
+        url: baseurl + 'events',
+        templateUrl: 'templates/events.html',
+        controller: 'eventsViewModel'
+    })
+
+    // bio
+    .state('bio', {
+        url: baseurl + 'bio',
+        templateUrl: 'templates/bio.html',
+        controller: 'bioViewModel'
+    })
+		
+	// music
+    .state('music', {
+    	url: baseurl + 'music',
+    	templateUrl: 'templates/music.html',
+    	controller: 'musicViewModel'
+    })
+
+	// photos
+    .state('photos', {
+    	url: baseurl + 'photos',
+    	templateUrl: 'templates/photos.html',
+    	controller: 'photosViewModel'
+    })
+
+	// videos
+    .state('videos', {
+    	url: baseurl + 'videos',
+    	templateUrl: 'templates/videos.html',
+    	controller: 'videosViewModel'
+    })
+
+    // contact
+    .state('contact', {
+    	url: baseurl + 'contact',
+    	templateUrl: 'templates/contact.html',
+    	controller: 'contactViewModel'
+    });
+
+});
+
+//----------------------------------------//
+appMainModule.controller("indexViewModel", function ($scope, $http, $location) {
+	$scope.heading = '';
+	$scope.headingCaptionBase = 'DJ Fresh';
+	$scope.headingCaption = $scope.headingCaptionBase;
+
+
+	$scope.$on('pageChange', function (event, menu) {
+		$scope.heading = menu;
+		$scope.headingCaption = $scope.headingCaptionBase + ' - ' + menu;
+
+	});
+
+	$scope.getNavMenuClass = function (name) {
+		return ($scope.heading.toLowerCase() == name.toLowerCase()) ? 'active' : 'inactive';
+	};
+
+});
+
+//----------------------------------------//
+appMainModule.controller("homeViewModel", function ($scope, $http, $location) {
+	$scope.$emit('pageChange', 'Home');
+});
+
+//----------------------------------------//
+appMainModule.controller("eventsViewModel", function ($scope, $http, $location) {
+	$scope.$emit('pageChange', 'Events');
+});
+
+//----------------------------------------//
+appMainModule.controller("bioViewModel", function ($scope, $http, $location) {
+	$scope.$emit('pageChange', 'Bio');
+});
+
+//----------------------------------------//
+appMainModule.controller("musicViewModel", function ($scope, $http, $location) {
+	$scope.$emit('pageChange', 'Music');
+});
+
+//----------------------------------------//
+appMainModule.controller("photosViewModel", function ($scope, $http, $location) {
+	$scope.$emit('pageChange', 'Photos');
+});
+
+//----------------------------------------//
+appMainModule.controller("videosViewModel", function ($scope, $http, $location) {
+	$scope.$emit('pageChange', 'Videos');
+});
+
+//----------------------------------------//
+appMainModule.controller("contactViewModel", function ($scope, $timeout) {
+	$scope.$emit('pageChange', 'Contact');
+
+	$scope.onSend = function () {
+		$scope.isProcessing = true;
+		generateInfoMessage('Send button clicked', 5000);
+
+		$timeout(function () {
+			$scope.isProcessing = false;
+		}, 5000);
+	};
+
+});
+
+
---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+﻿/**
+ *  
+ */
+
+//'use strict';
+
+//----------------------------------------//
+function generate(type, text) {
+
+	var n = noty({
+		text: text,
+		type: type,
+		dismissQueue: true,
+		layout: 'topRight',
+		closeWith: ['click'],
+		theme: 'relax',
+		maxVisible: 10,
+		animation: {
+			open: 'animated bounceInRight',
+			close: 'animated bounceOutRight',
+			easing: 'swing',
+			speed: 250
+		}
+	});
+
+	//console.log('html: ' + n.options.id);
+
+	return n;
+}
+
+//----------------------------------------//
+function generateInfoMessage(message, seconds) {
+	var noty = generate('warning', '<div class="activity-item font600"> <i class="fa fa-info text-warning"></i> <div class="activity"> ' + message + ' </div> </div>');
+	setTimeout(function () { $.noty.close(noty.options.id); }, seconds);
+}
+
+//----------------------------------------//
+$(document).ready(function () {
+
+	generateInfoMessage('Playing in Annandale, VA this Saturday night 7/25. Open bar until 9PM. <a href="http://www.google.com">Come check it out.</a>.', 7000);
+
+});
